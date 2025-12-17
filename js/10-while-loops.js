d7c104c5022adbbaf63bab0030176f409e964a31
wasimabass <wasimabass@gmail.com>
2025-12-17 05:36:38 +0500

    Update index.html to comment out unused scripts and modify while loop implementation in 10-while-loops.js to use a do-while structure.

--- js/10-while-loops.js
+++ js/10-while-loops.js
@@ -8,3 +8,10 @@ while(i < n){
     console.log(i);
     i++;
 }
+
+async function fetchData(){
+    const response = await fetch('https://wordpress-1398297-5215507.cloudwaysapps.com/wp-json/imblogs/v1/single/blog/?link=/lifestyle/how-to-check-etisalat-data-balance/');
+    const data = await response.json();
+    console.log(data);
+}
+fetchData();
